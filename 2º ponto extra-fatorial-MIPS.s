06156cd49af5cf31c5ceee1d86a0a239e97407ba
Luigi Muller <sluigimuller@gmail.com>
2018-09-04 00:18:54 -0400

    2º ponto extra - AOC UFRR 2018.2 - fatorial
    Retirei os caracteres acentuados que geravam erro na compilação, adicionei strings para serem impressas na tela

--- 2º ponto extra-fatorial-MIPS.s
+++ 2º ponto extra-fatorial-MIPS.s
@@ -1,33 +1,56 @@
 # Title: Fatorial de n
 # Author: Luigi Muller Sousa Linhares
-# Matrícula: 2017009506
-# Description: Calcula o fatorial de n
+# Matricula: 2017009506
+# Descricao: Calcula o fatorial de n
 # Input: o inteiro n
 # Output: o fatorial de n
-################# Data segment #####################
+
+############### Code Segment ####################
 .data
-    ent: .asciiz "Insira o valor: "
-    sai: .asciiz "Fatorial: "
-################# Code segment #####################
+	ent: .asciiz "Insira o valor: "
+	sai: .asciiz "Fatorial: "
+############### Data Segment ####################
 .text
 .globl main
-main: # main program entry
-    li $v0, 4 
-    li $v0, 10          # Exit program
-    syscall
+main:
+	
+	la $a0, ent
+	li $v0, 4
+	syscall
+	
+	li $v0, 5
+	syscall
+	
+	move $a0, $v0
+	
+	jal fact
+	
+	move $t0, $v0
+	
+	la $a0, sai
+	li $v0, 4
+	syscall
+	
+	move $a0, $t0
+	li $v0, 1
+	syscall
+	
+	li $v0, 10
+	syscall
+	
 fact:
-    slti $t0,$a0,2      # (n<2)?
-    beq $t0,$0,else     # if false branch to else
-    li $v0,1 # $v0 = 1
-    jr $ra # return to caller
+	slti $t0, $a0, 2
+	beq $t0, $0, else
+	li $v0, 1
+	jr $ra
 else:
-    addiu $sp,$sp,-8    # allocate 2 words on stack
-    sw $a0,4($sp)       # save argument n
-    sw $ra,0($sp)       # save return address
-    addiu $a0,$a0,-1    # argument = n-1
-    jal fact # call fact(n-1)
-    lw $a0,4($sp)       # restore argument
-    lw $ra,0($sp)       # restore return address
-    mul $v0,$a0,$v0     # $v0 = n*fact(n-1)
-    addi $sp,$sp,8      # free stack frame
-    jr $ra # return to caller 
+	addiu $sp, $sp, -8
+	sw $a0, 4($sp)
+	sw $ra, 0($sp)
+	addiu $a0, $a0, -1
+	jal fact
+	lw $a0, 4($sp)
+	lw $ra, 0($sp)
+	mul $v0, $a0, $v0
+	addiu $sp, $sp, 8
+	jr $ra
